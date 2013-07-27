9a52d7aa9d48cd19b51fc8e03c56db798ea2560a
Adnan Abdulhussein <adnan@prydoni.us>
2013-07-27 11:12:19 +0100

    Modify dotjs to load script in document body
    This allows the loaded script to access the window object, rather than the sandboxed window object that eval gives.

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,10 +1,4 @@
-$.ajax({
-  url: 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js',
-  dataType: 'text',
-  success: function(d){
-    $(function(){ eval(d) })
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
-  }
-})
+var script = document.createElement( 'script' );
+script.type = 'text/javascript';
+script.src = 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js';
+document.body.appendChild(script);
